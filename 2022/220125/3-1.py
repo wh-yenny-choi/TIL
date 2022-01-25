ca2a0ff62fad2dec67adf9d1449ed841e0753299
yenny <choiwonhee.data@gmail.com>
2022-01-25 21:27:05 +0900

    til

---
+++ 2022/220125/3-1.py
@@ -0,0 +1,10 @@
+n = 1260
+count = 0  # 거스름돈
+
+coins = [500, 100, 50, 10]
+
+for coin in coins:
+    count += n // coin  # 카장 큰 화폐 단위부터 거슬러 줄 수있는 거스름돈의 갯수
+    n %= coin  # 거슬러 준 이후의 남은 거스름돈
+
+print(count)
